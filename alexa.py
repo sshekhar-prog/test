7b0d201d5c3d662469b1181db489946eb0afaa38
shekhar sharma <74219514+sshekhar-prog@users.noreply.github.com>
2021-01-20 04:42:42 -0800

    Create alexa.py
    romentic alexa

---
+++ alexa.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener= sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty("voice",voices[1].id)
+engine.say("i am your alexa")
+engine.say('what can i do for you')
+engine.runAndWait()
+
+try:
+    with sr.Microphone() as source:
+        print("listening...")
+        voice=listener.listen(source)
+        command=listener.recognize_google(voice)
+        command=command.lower()
+        if 'alexa' in command:
+        print(command)
+                                                                   
+except:
+    pass        
